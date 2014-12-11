5225363b5ca25a9416e649aec417cd018ccddea2
Alexander Farley <info@traffic-camera.com>
2014-12-10 22:43:47 -0500

    eliminate PerCar setting

---
+++ OptAssignTest/CircleVehicles.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+namespace OptAssignTest
+{
+    /// <summary>
+    /// Generate "video" frames of circles moving by trajectories.
+    /// </summary>
+    public class CircleVehicles : AnimationBase
+    {
+        /// <summary>
+        /// Size of the vehicle.
+        /// </summary>
+        public int VehicleSize
+        {
+            get { return _vehicleSize; }
+            set { _vehicleSize = value; }
+        }
+        private int _vehicleSize = 3;
+
+        /// <summary>
+        /// Color of the vehicle.
+        /// </summary>
+        public Bgr VehicleColor
+        {
+            get { return _vehicleColor; }
+            set { _vehicleColor = value; }
+        }
+        private Bgr _vehicleColor = new Bgr(Color.White);
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="width">Frame width.</param>
+        /// <param name="height">Frame height.</param>
+        /// <param name="trajectories">Steps with coordinates for multiple vehicles.</param>
+        public CircleVehicles(int width, int height, IEnumerable<Point[]> trajectories) : base(width, height, trajectories)
+        {
+        }
+
+        protected override void DrawVehicle(Image<Bgr, byte> image, Point point, int index)
+        {
+            image.Draw(new CircleF(new PointF(point.X, point.Y), VehicleSize), VehicleColor, 0);
+        }
+    }
+}
---
+++ OptAssignTest/TrajectoryDetectionTest.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VTC;
+
+namespace OptAssignTest
+{
+    [TestClass]
+    public class TrajectoryDetectionTest
+    {
+        // shared settings
+        private static readonly TestSettings _settings = new TestSettings();
+
+        [TestMethod]
+        public void EmptyTrajectory_ShouldNotDetectVehicles()
+        {
+            var vista = CreateVista();
+
+            var generator = new CircleVehicles((int)_settings.FrameWidth, (int)_settings.FrameHeight, Enumerable.Empty<Point[]>());
+            foreach (var frame in generator.Frames())
+            {
+                vista.Update(frame);
+                Assert.AreEqual(0, vista.LastDetectionCount); 
+            }
+        }
+
+        [TestMethod]
+        public void SingleDiagonalTrajectory_ShouldDetectSingleVehicle()
+        {
+            var vista = CreateVista();
+
+            var diagonal = Enumerable.Range(5, 195).Select(x => new[] { new Point(x, x) });
+
+            int count = 0;
+            var generator = new CircleVehicles((int) _settings.FrameWidth, (int) _settings.FrameHeight, diagonal);
+            foreach (var frame in generator.Frames())
+            {
+                vista.Update(frame);
+                if (count++ > 2) // ignoring first iterations, since it saves background (first step) and accumulates statistics(?).
+                {
+                    Assert.AreEqual(1, vista.CurrentVehicles.Count);
+                }
+            }
+        }
+
+        [TestMethod]
+        public void TwoDiagonalsTrajectories_ShouldDetectTwoVehicles()
+        {
+            var vista = CreateVista();
+
+            var diagonals = Enumerable.Range(5, 195).Select(x => new[] { new Point(x, x + 5), new Point(x, x - 5) });
+
+            int count = 0;
+            var generator = new CircleVehicles((int) _settings.FrameWidth, (int) _settings.FrameHeight, diagonals);
+            foreach (var frame in generator.Frames())
+            {
+                vista.Update(frame);
+                if (count++ > 5) // ignoring first iterations, since it saves background (first step) and accumulates statistics(?).
+                {
+                    Assert.AreEqual(2, vista.CurrentVehicles.Count,"Expected: 2, Actual: " + vista.CurrentVehicles.Count + " Failed on step " + count.ToString() );
+                }
+            }
+        }
+
+        /// <summary>
+        /// Creates initialized intersection vista to be used for tests.
+        /// </summary>
+        /// <returns></returns>
+        private static IntersectionVista CreateVista()
+        {
+            // create mask for the whole image
+            var polygon = new Polygon();
+            polygon.AddRange(new[]
+                            {
+                                new Point(0, 0), 
+                                new Point(0, (int) _settings.FrameHeight),
+                                new Point((int) _settings.FrameWidth, (int) _settings.FrameHeight), 
+                                new Point((int) _settings.FrameWidth, 0),
+                                new Point(0, 0)
+                            });
+
+            var regionConfig = new RegionConfig
+                                {
+                                    RoiMask = polygon
+                                };
+
+            return new IntersectionVista(_settings, (int) _settings.FrameWidth, (int) _settings.FrameHeight)
+                    {
+                        RegionConfiguration = regionConfig
+                    };
+        }
+    }
+} ;
