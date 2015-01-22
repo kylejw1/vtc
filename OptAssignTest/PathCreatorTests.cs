4fff25d689b27acc3098b84a886abaa10f207aed
Eugene Rymski <erymski@gmail.com>
2015-01-23 01:14:59 +0300

    Tests for straight movements.

--- OptAssignTest/PathCreatorTests.cs
+++ OptAssignTest/PathCreatorTests.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System;
+using System.Drawing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace OptAssignTest
 {
@@ -52,6 +54,81 @@ namespace OptAssignTest
             SingleHorizontalPathTest(Direction.East);
         }
 
+        [TestMethod]
+        [Description("Check points generated for South->North path.")]
+        public void FromSouthDirection()
+        {
+            VerifyDirection(Direction.South, delegate(Point currPos, Point prevPos)
+                                                {
+                                                    Assert.AreEqual(currPos.X, prevPos.X, "No change in X position is expected.");
+                                                    Assert.IsTrue(currPos.Y > prevPos.Y, "The path should go up.");
+                                                });
+        }
+
+        [TestMethod]
+        [Description("Check points generated for North->South path.")]
+        public void FromNorthDirection()
+        {
+            VerifyDirection(Direction.North, delegate(Point currPos, Point prevPos)
+                                                {
+                                                    Assert.AreEqual(currPos.X, prevPos.X, "No change in X position is expected.");
+                                                    Assert.IsTrue(currPos.Y < prevPos.Y, "The path should go down.");
+                                                });
+        }
+
+        [TestMethod]
+        [Description("Check points generated for East->West path.")]
+        public void FromEastDirection()
+        {
+            VerifyDirection(Direction.East, delegate(Point currPos, Point prevPos)
+                                                {
+                                                    Assert.AreEqual(currPos.Y, prevPos.Y, "No change in Y position is expected.");
+                                                    Assert.IsTrue(currPos.X < prevPos.X, "The path should go left.");
+                                                });
+        }
+
+        [TestMethod]
+        [Description("Check points generated for West->East path.")]
+        public void FromWestDirection()
+        {
+            VerifyDirection(Direction.West, delegate(Point currPos, Point prevPos)
+                                                {
+                                                    Assert.AreEqual(currPos.Y, prevPos.Y, "No change in Y position is expected.");
+                                                    Assert.IsTrue(currPos.X > prevPos.X, "The path should go right.");
+                                                });
+        }
+
+        private static void VerifyDirection(Direction direction, Action<Point, Point> validation)
+        {
+            var settings = CreateSettings(VehicleRadius);
+
+            IPathGenerator generator = PathCreator
+                .New(settings)
+                .StraightFrom(direction);
+
+            Point? prevPoint = null;
+            for (uint frame = 0; !generator.IsDone(frame); frame++)
+            {
+                Point? position = generator.GetPosition(frame);
+                if (! position.HasValue) continue;
+
+                if (prevPoint.HasValue)
+                {
+                    validation(position.Value, prevPoint.Value);
+                }
+                prevPoint = position;
+            }
+        }
+
+        [TestMethod]
+        [Description("Empty script should behave as a completed script.")]
+        public void EmptyScriptCompleteness() 
+        {
+            var script = new Script();
+
+            Assert.IsTrue(script.IsDone(0), "Should be done for beginning.");
+        }
+
         private static void SingleVerticalPathTest(Direction direction)
         {
             var settings = CreateSettings(VehicleRadius);
@@ -76,15 +153,6 @@ namespace OptAssignTest
             Assert.IsFalse(script.IsDone(0), "Should not be done for beginning.");
             Assert.IsFalse(script.IsDone(settings.HorizontalPathLength() - 1), "Should not be done for last point.");
             Assert.IsTrue(script.IsDone(settings.HorizontalPathLength()), "Should be done for next after the last point.");
-        }
-
-        [TestMethod]
-        [Description("Empty script should behave as a completed script.")]
-        public void EmptyScriptCompleteness() 
-        {
-            var script = new Script();
-
-            Assert.IsTrue(script.IsDone(0), "Should be done for beginning.");
         }
     }
 }
