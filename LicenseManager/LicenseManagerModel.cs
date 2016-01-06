108bc289d6ebe40a4eca135a546f0b0e71c5c993
Kyle Wagner <kylejw1@gmail.com>
2016-01-05 22:21:50 -0700

    Started the license manager

---
+++ LicenseManager/LicenseManagerModel.cs
@@ -0,0 +1,20 @@
+﻿using SkyXoft.BusinessSolutions.LicenseManager.Protector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LicenseManager
+{
+    public class LicenseManagerModel
+    {
+        public ExtendedLicense License { get; private set; }
+
+        public void SetLicenseKey(string key)
+        {
+            License = ExtendedLicenseManager.GetLicense(key);
+        }
+
+    }
+}
