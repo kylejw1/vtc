fe3567bed037cb346746055c67af6942daee8d6b
Kyle Wagner <kylejw1@gmail.com>
2016-01-05 22:21:50 -0700

    Started the license manager

---
+++ LicenseManager/LicenseManagerController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LicenseManager
+{
+    public class LicenseManagerController
+    {
+        LicenseManagerModel _model;
+        ILicenseManagerView _view;
+
+        public LicenseManagerController(LicenseManagerModel model, ILicenseManagerView view)
+        {
+            _view = view;
+            _model = model;
+
+            _view.SetController(this);
+        }
+
+        public void SetLicenseKey(string key)
+        {
+            _model.SetLicenseKey(key);
+
+            if (_model.License.Validate())
+            {
+
+            } else
+            {
+
+            }
+        }
+    }
+}
