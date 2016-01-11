9b4fe6365c29b9e324dccfc318cfac361c8e0847
Kyle Wagner <kylejw1@gmail.com>
2016-01-10 21:15:00 -0700

    License Management

---
+++ LicenseManagerTests/LicenseManagerControllerTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LicenseManager;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LicenseManager.Tests
+{
+    [TestClass()]
+    public class LicenseManagerControllerTests
+    {
+        private const string ValidLicenseKey = "D2DF-1C8F-6A5C-40F0-8E5F-041B-9A8A";
+        private const string InvalidLicenseKey = "D2DF-1C8F-6A5C-40F0-8E5F-041B-9999";
+
+        [TestMethod()]
+        public void SetLicenseKeyTest()
+        {
+            var model = new LicenseManagerModel();
+            var controller = new LicenseManagerController(model, null);
+
+            Assert.IsTrue(controller.TrySetLicenseKey(ValidLicenseKey));
+
+            Assert.IsFalse(controller.TrySetLicenseKey(InvalidLicenseKey));
+        }
+    }
+}
